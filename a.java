0d14ace9fbbfc509d33a4e84aabe5f9472fbdbd2
Sarbojit Roy <sarbojitsroy7409@gmail.com>
2024-03-23 13:31:42 +0530

    make a program to find even and odd numbers from 1 to 20

---
+++ a.java
@@ -0,0 +1,12 @@
+public class a {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println("even");
+            } else {
+                System.out.println("odd");
+            }
+        }
+    }
+}
